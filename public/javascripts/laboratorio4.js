151fd6be31c7264afaf18cf1f55d835430635b17
jasgs <jasgs@DESKTOP-FT1HJ5N>
2017-10-29 12:40:10 -0600

    Se ha compleado la función para añadir pizzas. Ahora es posible añadir una pizza y ver el resultado en la pantalla principal de las pizzas. Faltan corregir errores visuales y hacer pruebas..

--- public/javascripts/laboratorio4.js
+++ public/javascripts/laboratorio4.js
@@ -4,3 +4,42 @@ function loadJWT()
     var content = document.getElementById('JSON_CONTENT').value;
     document.location.href=  "/JWT/" + password +"&&"+ content;
 }
+
+function loadPizzas(pizzaJSON)
+{
+    console.log(pizzaJSON);
+    var pizzas = JSON.parse(pizzaJSON);
+    //Obtenemos la tabla
+    var table = document.getElementById("pizzasInfo").getElementsByTagName("tbody")[0];
+    
+    // Constantes 
+    let btn_edit = '<button type="submit" class="btn btn-warning m-1">Editar</button>';
+    let btn_delete = '<button type="submit" class="btn btn-danger m-1">Borrar</button>';
+    let form_edit = '<form action="Edit" method="get">';
+    let form_delete = '<form action="Delete" method="post">';
+    let hidden = '<input type="hidden" value="';
+    let hiddenf = '">';
+
+    for(var pizza in pizzas)
+    {
+        //Insertamos una nueva fila
+        var row = table.insertRow(0);
+        //Insertamos las celdas
+        var pizzaName =  row.insertCell(0);
+        var pizzaDesc =  row.insertCell(1);
+        var pizzaMix =  row.insertCell(2);
+        var pizzaMass=  row.insertCell(3);
+        var pizzaCheese =  row.insertCell(4);
+        var pizzaPieces =row.insertCell(5);
+        var pizzaOptions =  row.insertCell(6);
+
+        pizzaName.innerHTML = pizzas[pizza].name;
+        pizzaDesc.innerHTML = pizzas[pizza].desc;
+        pizzaMix.innerHTML = pizzas[pizza].mix;
+        pizzaMass.innerHTML = pizzas[pizza].mass;
+        pizzaCheese.innerHTML = pizzas[pizza].cheese === "1" ? "Sí":'No';
+        pizzaPieces.innerHTML = pizzas[pizza].pieces;
+        pizzaOptions.innerHTML = form_edit + hidden +  pizzas[pizza].name + hiddenf + btn_edit + '</form>';
+        pizzaOptions.innerHTML += form_delete + hidden +  pizzas[pizza].name + hiddenf + btn_delete + '</form>';
+    }
+}
--- routes/pizzaCRUD.js
+++ routes/pizzaCRUD.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-let pizzas = [];
+let pizzas = {};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mainPizza', { title: 'Pizza CRUD', name: 'Pizza CRUD' });
+  console.log("THIS IS THE JSON TO SEND ----> " + JSON.stringify(pizzas));
+  res.render('mainPizza', { title: 'Pizza CRUD', name: 'Pizza CRUD', rows : JSON.stringify(pizzas) });
 });
 
 router.get('/Create', function(req, res, next) {
   res.render('createPizza', { title: 'Pizza CRUD', name: 'Pizza CRUD' });
 });
 
-router.post('/Created', function(req, res, next) {
-  res.render('mainPizza', { title: 'Pizza CRUD', name: 'Pizza CRUD' });
+router.post('/Created', function(req, res, next)
+ {
+  pizzas[req.body.pizzaName] = { name : req.body.pizzaName, desc : req.body.pizzaDesc, mix : req.body.mix, mass : req.body.masa, cheese : req.body.queso, pieces : req.body.porciones };
+  console.log("this is the name ---> " + req.body.pizzaName);
+  console.log("this is the object ---> " + pizzas.length);
+  res.render('mainPizza', { title: 'Pizza CRUD', name: 'Pizza CRUD', rows : JSON.stringify(pizzas)});
 });
 
 module.exports = router;
