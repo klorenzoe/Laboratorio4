ba0f06b6773fc42a160f43ca3b4efaf1ea117451
jasgs <jasgs@DESKTOP-FT1HJ5N>
2017-10-30 20:22:52 -0600

    Search actualizado y listo para completar

--- routes/pizzaCRUD.js
+++ routes/pizzaCRUD.js
@@ -18,9 +18,25 @@ router.get('/Delete', function(req, res, next) {
   if (pizzas[req.query.name] !== undefined){
     delete pizzas[req.query.name];
   }
-  console.log("INPUT ----> " + pizzas);
   res.render('messagePizza', { title: 'Pizza CRUD', name: "Pizza CRUD",  message1: 'Se ha eliminado correctamente tu pizza: ', message2 : req.query.name });
 });
+
+router.get('/Search', function(req, res, next)
+{
+  //recuperamos la variable name, que se envia desde el searchPizza en el unico form
+  var pizzaName = req.query.name;
+  //creamos una nueva lista de pizzas que encajen con la busqueda
+  var subPizzas = {}
+  //Se busca en todos los elementos de pizzas
+  for (var pizzaKey in pizzas)
+  {
+    // haces tu algoritmo
+    // si una busqueda encaja lo guardamos asi
+    // subPizzas[pizzaKey] = pizzas[pizzasKey];
+  }
+ res.render('searchPizza', { title: 'Pizza CRUD', name: 'Pizza CRUD', /*Este guarda el valor que se busco y lo manda al input type='text'*/ word : ''/*en lugar de las comillas debe ir req.query.name*/, message1: 'Este es un mensaje pequeño', message2 : 'Este es uno grande...',rows : /*En lugar de retornar pizzas, retornamos subPizzas*/JSON.stringify(pizzas)});
+});
+
 
 router.post('/Created', function(req, res, next)
  {
