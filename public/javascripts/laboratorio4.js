4fb5b16e23ad65e51e7e3717ffa92fea0dee6976
LAPTOP-QSGRN4CF\TonyTaze <tonytaze096@gmail.com>
2017-10-31 19:01:22 -0600

    Editar

--- public/javascripts/laboratorio4.js
+++ public/javascripts/laboratorio4.js
@@ -5,6 +5,103 @@ function loadJWT()
     document.location.href=  "/JWT/" + password +"&&"+ content;
 }
 
+function updatePizzas(pizzaJSON) {
+    console.log(pizzaJSON);
+    var pizza = JSON.parse(pizzaJSON);
+    document.getElementById('pizzaName').value = pizza.name;
+    document.getElementById('pizzaDesc').value = pizza.desc;
+    var ingrediente = document.getElementsByName('mix');
+
+    console.log(typeof pizza.mix === "string");
+    if (typeof pizza.mix === "string") {
+            switch (pizza.mix) {
+                case "Jamón":
+                    document.getElementById('jamon').checked = true;
+                    break;
+                case "Peperoni":
+                    document.getElementById('pepe').checked = true;
+                    break;
+                case "Champiñones":
+                    document.getElementById('champ').checked = true;
+                    break;
+                case "Pimientos":
+                    document.getElementById('pimi').checked = true;
+                    break;
+                case "Cebolla":
+                    document.getElementById('cebol').checked = true;
+                    break;
+                case "Anchoas":
+                    document.getElementById('anch').checked = true;
+                    break;
+                case "Piña":
+                    document.getElementById('pina').checked = true;
+                    break;
+                case "Tomates":
+                    document.getElementById('tomat').checked = true;
+                    break;
+                case "Albahaca":
+                    document.getElementById('albah').checked = true;
+                    break;
+                default:
+                    break;
+            }
+    } else {
+        for (var i in pizza.mix) {
+            console.log(pizza.mix[i]);
+            console.log(i);
+            switch (pizza.mix[i]) {
+                case "Jamón":
+                    document.getElementById('jamon').checked = true;
+                    break;
+                case "Peperoni":
+                    document.getElementById('pepe').checked = true;
+                    break;
+                case "Champiñones":
+                    document.getElementById('champ').checked = true;
+                    break;
+                case "Pimientos":
+                    document.getElementById('pimi').checked = true;
+                    break;
+                case "Cebolla":
+                    document.getElementById('cebol').checked = true;
+                    break;
+                case "Anchoas":
+                    document.getElementById('anch').checked = true;
+                    break;
+                case "Piña":
+                    document.getElementById('pina').checked = true;
+                    break;
+                case "Tomates":
+                    document.getElementById('tomat').checked = true;
+                    break;
+                case "Albahaca":
+                    document.getElementById('albah').checked = true;
+                    break;
+                default:
+                    break;
+            }
+        } 
+    }
+    var radio = document.getElementsByName('masa');
+    switch (pizza.mass) {
+        case "Delgada":
+            radio[0].checked = true;
+            break;
+        case "Normal":
+            radio[1].checked = true;
+            break;
+        case "Pan":
+            radio[2].checked = true;
+            break;
+    }
+
+    if (pizza.cheese === "1")
+        document.getElementsByName('queso')[0].checked = true;
+
+    document.getElementById('porciones').value = pizza.pieces;
+}
+
+
 function loadPizzas(pizzaJSON)
 {
     console.log(pizzaJSON);
@@ -12,7 +109,7 @@ function loadPizzas(pizzaJSON)
     //Obtenemos la tabla
     var table = document.getElementById("pizzasInfo").getElementsByTagName("tbody")[0];
     
-    // Constantes 
+    // Constantes  
     let btn_edit = '<button type="submit" class="btn btn-warning m-1">Editar</button>';
     let btn_delete = '<button type="submit" class="btn btn-danger m-1">Borrar</button>';
     let form_edit = '<form action="pizzaCRUD/Update" method="get">';
